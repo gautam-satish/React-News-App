73659ec3715cfa696ad2993dc60c9b2febeb5ebc
“Gautam” <gautam@paperflite.com>
2023-10-04 10:45:16 +0530

    Added sample json and looping

--- src/components/News.js
+++ src/components/News.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NewsItem from './NewsItem'
 
 export default function News() {
+
+  const [sampleData, setSampleData] = useState([{
+    title: "My Title",
+    description: "My Description",
+    imageUrl: "https://fastly.picsum.photos/id/216/200/300.jpg?hmac=c3OXbiUxWPMgwnaFpX8ZAfBL5TZzWjnof6mb4OwuSPs"
+  },{
+    title: "My Title2",
+    description: "My Description",
+    imageUrl: "https://fastly.picsum.photos/id/216/200/300.jpg?hmac=c3OXbiUxWPMgwnaFpX8ZAfBL5TZzWjnof6mb4OwuSPs"
+  },{
+    title: "My Title3",
+    description: "My Description",
+    imageUrl: "https://fastly.picsum.photos/id/216/200/300.jpg?hmac=c3OXbiUxWPMgwnaFpX8ZAfBL5TZzWjnof6mb4OwuSPs"
+  }]);
+
+  // const setData = ({
+  //   setSampleData
+  // })
+
+
+  // let sampleJson = [{
+  //   title: "My Title",
+  //   description: "My Description",
+  //   imageUrl: "https://fastly.picsum.photos/id/216/200/300.jpg?hmac=c3OXbiUxWPMgwnaFpX8ZAfBL5TZzWjnof6mb4OwuSPs"
+  // },{
+  //   title: "My Title2",
+  //   description: "My Description",
+  //   imageUrl: "https://fastly.picsum.photos/id/216/200/300.jpg?hmac=c3OXbiUxWPMgwnaFpX8ZAfBL5TZzWjnof6mb4OwuSPs"
+  // },{
+  //   title: "My Title3",
+  //   description: "My Description",
+  //   imageUrl: "https://fastly.picsum.photos/id/216/200/300.jpg?hmac=c3OXbiUxWPMgwnaFpX8ZAfBL5TZzWjnof6mb4OwuSPs"
+  // }]
+
   return (
     <>
-    <div className="my-3 mx-3">
-        <NewsItem imageUrl="https://fastly.picsum.photos/id/216/200/300.jpg?hmac=c3OXbiUxWPMgwnaFpX8ZAfBL5TZzWjnof6mb4OwuSPs" title="The title is awesome" description="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."/>
+    <div className="container my-3 mx-3">
+      <div className='row'>
+        {sampleData.map((element)=> {
+         return <div className='col md-4'>
+              <NewsItem imageUrl={element.imageUrl} title={element.title} description={element.description}/>
+          </div>  
+        })} 
+      </div>
     </div>
     </>
   )

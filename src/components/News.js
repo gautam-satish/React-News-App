63ecda6a19ae7635b49442ad98522836c9eff8e7
“Gautam” <gautam@paperflite.com>
2023-10-05 14:43:04 +0530

    Added useEffect(didMount)

--- src/components/News.js
+++ src/components/News.js
@@ -1,30 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import NewsItem from './NewsItem'
 
 export default function News() {
 
-  const [sampleData, setSampleData] = useState([{
+  const data = [{
     title: "My Title",
     description: "My Description",
-    imageUrl: "https://fastly.picsum.photos/id/216/200/300.jpg?hmac=c3OXbiUxWPMgwnaFpX8ZAfBL5TZzWjnof6mb4OwuSPs"
+    urlToImage: "https://fastly.picsum.photos/id/216/200/300.jpg?hmac=c3OXbiUxWPMgwnaFpX8ZAfBL5TZzWjnof6mb4OwuSPs",
+    url: ""
   },{
     title: "My Title2",
     description: "My Description",
-    imageUrl: "https://fastly.picsum.photos/id/216/200/300.jpg?hmac=c3OXbiUxWPMgwnaFpX8ZAfBL5TZzWjnof6mb4OwuSPs"
+    urlToImage: "https://fastly.picsum.photos/id/216/200/300.jpg?hmac=c3OXbiUxWPMgwnaFpX8ZAfBL5TZzWjnof6mb4OwuSPs",
+    url: "https://youtube.com"
   },{
     title: "My Title3",
     description: "My Description",
-    imageUrl: "https://fastly.picsum.photos/id/216/200/300.jpg?hmac=c3OXbiUxWPMgwnaFpX8ZAfBL5TZzWjnof6mb4OwuSPs"
-  }]);
-  
+    urlToImage: "https://fastly.picsum.photos/id/216/200/300.jpg?hmac=c3OXbiUxWPMgwnaFpX8ZAfBL5TZzWjnof6mb4OwuSPs",
+    url: "https://google.com"
+  }];
+
+  const [sampleData, setSampleData] = useState([]);
+
+  useEffect(() => {
+    setSampleData(data);
+    // fetch()
+  }, []);
+
 
   return (
     <>
     <div className="container my-3 mx-3">
       <div className='row'>
         {sampleData.map((element)=> {
-         return <div className='col md-4'>
-              <NewsItem imageUrl={element.imageUrl} title={element.title} description={element.description}/>
+         return <div className='col md-4' key={element.url}>
+              <NewsItem imageUrl={element.urlToImage} title={element.title} description={element.description} url={element.url}/>
           </div>  
         })} 
       </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NewsItem(props) {
   return (
    <>
-    <div className="card mb-3 text-bg-dark" style={{"max-width": "540px"}}>
+    <div className="card mb-3 text-bg-dark" style={{"maxwidth": "540px"}}>
         <div className="row g-0">
             <div className="col-md-4">
             <img src={props.imageUrl} className="img-fluid rounded-start" alt="..."/>
@@ -13,6 +13,7 @@ export default function NewsItem(props) {
                 <h5 className="card-title">{props.title}</h5>
                 <p className="card-text" style={{"color": "rgb(165, 165, 165)"}}>{props.description}</p>
                 <p className="card-text"><small className="text-body-secondary">Last updated 1 mins ago</small></p>
+                <a  href={props.url} target="_blank">Read more</a>
             </div>
             </div>
         </div>
